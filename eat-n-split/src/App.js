246b2b362fd5583edfb57c6d23a321da55a486c4
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-05-26 20:15:19 +0300

    eat-n-split

---
+++ eat-n-split/src/App.js
@@ -0,0 +1,142 @@
+import { useState } from "react";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function Button({ children, onclick }) {
+  return (
+    <button className="button" onClick={onclick}>
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+  function handleAddFriend(newFriend) {
+    setFriends([...friends, newFriend]);
+    setShowAddFriend(false);
+  }
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend handleAddFriend={handleAddFriend} />}
+        <Button
+          onclick={() => {
+            setShowAddFriend((show) => !show);
+          }}
+        >
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendsList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function FormAddFriend({ handleAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48?u=499476");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+    const newFriend = {
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+      id: id,
+    };
+    handleAddFriend(newFriend);
+    setName("");
+    setImage("");
+  }
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label htmlFor="">Friend name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="">Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label htmlFor="">Bill Value</label>
+      <input type="text" />
+      <label htmlFor="">Your expense</label>
+      <input type="text" />
+      <label htmlFor="">X's expense</label>
+      <input type="text" disabled />
+      <label htmlFor="">Who is paying the bill</label>
+      <select name="" id="">
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
---
+++ travel-list/src/components/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleDeleteAll() {
+    const confirmed = window.confirm("Are you sure to delete all");
+    if (!confirmed) return;
+    setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form handleAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        handleDeleteItem={handleDeleteItem}
+        handleToggleItem={handleToggleItem}
+        handleDeleteAll={handleDeleteAll}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
